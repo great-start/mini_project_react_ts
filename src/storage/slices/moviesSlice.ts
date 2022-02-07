c157520e5eaaca9b917410a68807c118b41cfeab
ev <zhenya.bill@gmail.com>
2022-02-07 12:56:45 +0200

    authorization

---
+++ src/storage/slices/moviesSlice.ts
@@ -0,0 +1,40 @@
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {IMovie} from "../../interfaces";
+import {moviesService} from "../../services/movies.service";
+
+interface IMoviesState {
+    movies: IMovie[]
+}
+
+const initialState: IMoviesState = {
+    movies: [],
+}
+
+export const getPopularMovies = createAsyncThunk(
+    'moviesSlice/getPopularMovies',
+    async (page:number,{dispatch,rejectWithValue}) => {
+        try {
+            const {data} = await moviesService.getPopular(page);
+            dispatch(setPopularMovies(data.movies));
+        } catch (e) {
+            rejectWithValue(e);
+        }
+    }
+)
+
+const moviesSlice = createSlice({
+    name: 'moviesSlice',
+    initialState,
+    reducers: {
+        setPopularMovies: (state, action:PayloadAction<IMovie[]>) => {
+
+        }
+    },
+    extraReducers: {
+        [getPopularMovies.pending]: (state) => {
+
+        }
+    }
+});
+
+export const {setPopularMovies} = moviesSlice.actions;
--- src/storage/storage.ts
+++ src/storage/storage.ts
@@ -1,8 +1,13 @@
-import {combineReducers, createStore} from "@reduxjs/toolkit";
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
 
 
 const rootReducer = combineReducers({
     
 });
 
-export const store = createStore(rootReducer);
+export const store = configureStore({
+    reducer: rootReducer
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = typeof store.dispatch;
