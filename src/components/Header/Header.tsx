bf6b922ed1d396116737ac85db135b53f48b2213
ev <zhenya.bill@gmail.com>
2022-02-09 16:13:30 +0200

    9.02 not bad

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,14 @@ import {NavLink} from "react-router-dom";
 
 import css from './Header.module.css';
 import {useAppDispatch, useAppSelector} from "../../hooks";
-import {setDefault, setGenre} from "../../storage";
+import {setDefault} from "../../storage";
 import DayNightSwitcher from "../DatNigthSwitcher/DayNightSwitcher";
 import { NavGenreList } from '../NavGenreList/NavGenreList';
-import {IGenre} from "../../interfaces";
 
 export const Header: FC = () => {
 
     const dispatch = useAppDispatch();
     const {switcher} = useAppSelector(state => state.switcherReducer);
-
-    // function handler(genre: IGenre) {
-    //     dispatch(setDefault());
-    //     dispatch(setGenre(genre.id));
-    // }
 
     return (
         <div className={switcher ? css.header_day : css.header_night}>
